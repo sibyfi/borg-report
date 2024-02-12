4180d94d2d4f1e65f1c9ece71923228c289f8975
George Rucker <glr3@protonmail.com>
2024-02-12 16:01:40 -0500

    updated docstring

--- borg-report.py
+++ borg-report.py
@@ -2,6 +2,8 @@
 """This script prints the details of all the
 archives in the borg repo as well as some of
 the details of the borg repo
+
+This script doesn't have any error checking...yet.
 """
 
 import subprocess
